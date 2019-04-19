00d24a31e6c2e0646e0bbc2ce83f7040f194406e
Space Zhong <spacezhong@spacedemacbook-pro.local>
2019-04-19 19:40:42 +0800

    Initial Commit

---
+++ EnDICTATION/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnDICTATION
+//
+//  Created by Space Zhong on 2019/4/19.
+//  Copyright © 2019年 Space Zhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EnDICTATION/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnDICTATION
+//
+//  Created by Space Zhong on 2019/4/19.
+//  Copyright © 2019年 Space Zhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
